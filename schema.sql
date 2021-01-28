4c37fbfc8434cd8530b3ce6ff3b7caeadc78668e
Jas-F <jasminer.franklin@gmail.com>
2021-01-28 10:48:53 -0500

    create tables and db

---
+++ schema.sql
@@ -0,0 +1,31 @@
+create database employeedb;
+use employeedb;
+
+create table employee(
+
+id int auto_increment,
+First_name varchar(30) not null,
+Last_name varchar(30) not null,
+role_id int not null,
+manager_id int,
+
+primary key(id)
+);
+
+create table department(
+
+id int auto_increment,
+name varchar(30) unique not null,
+
+primary key(id)
+);
+
+create table role(
+
+id int auto_increment,
+title varchar(30) unique not null,
+salary decimal not null	,
+department_id int not null,
+
+primary key(id)
+);
