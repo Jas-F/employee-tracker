7a190f7c31c334e7a7a7dae31436046f13be7e34
Jas-F <jasminer.franklin@gmail.com>
2021-01-28 11:37:58 -0500

    add department

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (name) values ('Accounting');
+INSERT INTO department (name) values ('Payroll');
+INSERT INTO department (name) values ('Sales');
+INSERT INTO department (name) values ('IT');
+
+
+gi
