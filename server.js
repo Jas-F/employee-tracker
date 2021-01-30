f524640ad4a1c356a58d19870785efb4b5b0d52e
Jas-F <jasminer.franklin@gmail.com>
2021-01-29 20:36:30 -0500

    established connection

--- server.js
+++ server.js
@@ -3,23 +3,20 @@ const express = require("express");
 
 const app = express();
 
-// var mysqlConnection = mysql.createConnection({
-//     host : "localhost",
-//     user : "root",
-//     password : "password",
-//     database : "Employee",
-//     multipleStatements : true
-// });
+// create connection 
+var Connection = mysql.createConnection({
+    host : "localhost",
+    user : "root",
+    password : "password",
+    database : "Employee",
+});
 
-// mysqlConnection.connect((err) => {
-//         if (!err) {
-//             console.log("connected");
-//         }
-
-//         else {
-//             console.log("failed");
-//         }
-//     })
+Connection.connect((err) => {
+        if (err) {
+            console.log("err");
+        }
+        console.log('connected');
+    })
 
 app.listen('3000', () => {
     console.log('started');
