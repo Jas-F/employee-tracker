813645b01c09d19bf2304372b3b239755e72d0d2
Jas-F <jasminer.franklin@gmail.com>
2021-01-29 20:02:40 -0500

    add packages

--- schema.sql
+++ schema.sql
@@ -3,34 +3,29 @@ create database employee;
 use employee;
 
 -- Create table  
-create table employee(
-
+create table employees
+(
 id int auto_increment,
 First_name varchar(30) not null,
 Last_name varchar(30) not null,
 role_id int not null,
 manager_id int,
-
 primary key(id)
 );
 
-create table department(
-
+create table department
+(
 id int auto_increment,
 name varchar(30) unique not null,
-
 primary key(id)
 );
 
-create table role(
-
+create table role
+(
 id int auto_increment,
 title varchar(30) unique not null,
 salary decimal not null	,
 department_id int not null,
-
 primary key(id)
 );
-
--- Insert Records.
 
